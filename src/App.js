1c3db534cc33cbef21e375e756309903969405cd
roro1053 <c1053rroro@gmail.com>
2021-02-05 20:40:11 +0900

    useEffectが走る

--- src/App.js
+++ src/App.js
@@ -5,9 +5,37 @@ function App() {
   const [images, setImages] = useState([]);
   const [text, setText] = useState('');
   const [query, setQuery] = useState('apple');
+  useEffect (()=>{
+    console.log('useEffectが走りました。');
+    fetch(`https://api.unsplassh.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
+    .then(response =>response.json())
+      .then(date => {
+        console.log(date)
+        setImages(date.results)
+      })
+  },[query])
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuery(text);
+    setText('');
+    console.log('onSummitが呼ばれました');
+  }
+
   return (
     <div className="App">
-
+      <div className="main">
+        <form onSubmit ={onSubmit}>
+          <input
+            type="text"
+            onChange={e => setText(e.target.value)}
+            value= {text}
+          />
+          <button type="sumbit">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
